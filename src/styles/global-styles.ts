1e1f6bf276365623be0294454e067581a73e949f
gbwlxhd97 <gbwlxhd97@naver.com>
2023-02-04 01:17:08 +0900

    chore (_app.tsx,_document.tsx): install package for style setting Etc - change tsconfig.json absolute path - change global css - add .babelrc for styled-components

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,22 @@
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+      ${reset}
+      * {
+        box-sizing: border-box;
+      }
+      body{
+        font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+        margin:0;
+        padding:0;
+        background-color: #fff;
+      }
+      a {
+        color: inherit;
+        text-decoration: none;
+        cursor: pointer;
+      }
+    `;
+
+export default GlobalStyle;
